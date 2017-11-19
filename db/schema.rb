b49148cd2163a5944a6c9c9dfe107e155b32d9e7
Sabina <thimassabrina@gmail.com>
2017-11-19 13:53:39 +0100

    text

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119110550) do
+ActiveRecord::Schema.define(version: 20171119115704) do
 
   create_table "ideas", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "texts", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.string "picture"
